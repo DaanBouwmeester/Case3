877ea30232cf0dcfa62c7c10b7d384296eb85f74
DaanBouwmeester <91375116+DaanBouwmeester@users.noreply.github.com>
2021-10-14 10:08:19 +0200

    Update Case3.py

--- Case3.py
+++ Case3.py
@@ -11,10 +11,10 @@ import json
 import plotly.figure_factory as ff
 import statsmodels.api as sm
 
-st.title("Dashboard over Elektrische auto's en laadpalen")
-st.text('''Welkom op ons dashboard! Op ons dashboard is te zien hoe een gemiddelde bezetting. 
+st.title("Dashboard over elektrische auto's en laadpalen")
+st.text('''Welkom op ons dashboard! Op ons dashboard is te zien hoe een gemiddelde bezetting 
 van een laadpaal eruit ziet. Daarnaast zijn er nog veel meer interessante dingen
-over laadpalen en elektrische voertuigen''')
+over laadpalen en elektrische voertuigen.''')
 st.header('Laadpaaldata')
 st.subheader('Histogram van de laadtijd')
 st.text('''Hier zie je een aantal observaties op de y-as en de tijd in uren op de x-as van de 
@@ -26,7 +26,13 @@ te switchen. Dit kan voor de verbonden tijd en de oplaadtijd. Bij de histogram
 van de oplaadtijd zie je dat de hoogste piek iets na 2 ligt en dat de meeste 
 waarden ook daarvoor liggen. Dit komt ook overeen met de mediaan en het 
 gemiddelde. Ook zien we dat de verbonden tijd meer tijd in uren nodig heeft 
-dan de oplaadtijd.''')
+dan de oplaadtijd.
+
+Gemiddeld in deze dataset is het zo dat in 65,7% van de tijd de auto's echt aan het 
+laden zijn terwijl ze aan de laadpaal staan. Dit betekend dat ongveer 35% van de tijd
+de auto aan de laadpaal staat terwijl deze niet meer oplaad. Dit percentage zou naar 
+beneden kunnen om te zorgen dat er meer laadpaal capaciteit is voor andere bestuurders
+van elektrische auto's''')
 
 laadpaaldata = pd.read_csv('laadpaaldata.csv')
 pd.set_option('display.max_columns', None)
